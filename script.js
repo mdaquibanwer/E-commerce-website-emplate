6c885cc36fd50eff24801ef2a963c7733943fb31
Aquib <mdaquibanwer@gmail.com>
2022-12-18 21:28:35 +0530

    about page done

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const slides = document.querySelectorAll(".slide")
 let count = 0;
+let sideBar = document.querySelector('.side-bar');
 
+document.querySelector('#menu-btn').onclick = () =>{
+    sideBar.classList.toggle('active');
+}
 
+document.querySelector('#close-side-bar').onclick = () =>{
+    sideBar.classList.remove('active');
+}
+document.querySelector('#search-btn').onclick = () =>{
+    search-form.classList.toggle('active');
+}
+
+// image slider logic
 slides.forEach((slide,index)=>{
     slide.style.left = `${index*100}%`
 })
@@ -25,3 +37,10 @@ const slideImage = ()=>{
         slide.style.transform = `translateX(-${count*100}%)`
     })
 }
+
+// accordian logic 
+document.querySelectorAll('.accordion-container .accordion').forEach(accordion =>{
+    accordion.onclick = () =>{
+        accordion.classList.toggle('active');
+    }
+});
